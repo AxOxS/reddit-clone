b26ed24f103c8ba003f88bf539121095a9e0f585
AxOxS <axoxs1@gmail.com>
2023-07-06 13:06:55 +0300

    Finish layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
-import '@/styles/globals.css'
+//import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '@/chakra/theme';
+import Layout from '@/components/Layout/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <Layout>
       <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
