fa57bf0412384bf3beb603d4636ce23fb22838d8
AxOxS <axoxs1@gmail.com>
2023-07-08 15:41:48 +0300

    Made searchbar responsive

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -1,39 +1,47 @@
 import { SearchIcon } from '@chakra-ui/icons';
-import { Flex, Input, InputGroup, InputLeftElement, InputRightElement } from '@chakra-ui/react';
+import { Flex, Input, InputGroup, InputLeftElement} from '@chakra-ui/react';
 import React from 'react';
+import { User } from 'firebase/auth';
 
 type SearchInputProps = {
-    //user:
+    user?: User | null;
 };
 
-const SearchInput: React.FC<SearchInputProps> = () => {
-
+const SearchInput: React.FC<SearchInputProps> = ({ user }) => {
     return (
-        <Flex flexGrow={1} mr={2} align='center'>
-            <InputGroup>
-                <InputLeftElement pointerEvents='none'>
-                    <SearchIcon color='gray.400' mb={1}/>
-                </InputLeftElement>
-                <Input 
-                    placeholder='Search Reddit' 
-                    fontSize='10pt' 
-                    _placeholder={{ color: 'gray.500'}}
-                    _hover={{
-                        bg: 'white',
-                        border: '1px solid',
-                        borderColor: 'blue.500',
-                    }}
-                    _focus={{
-                        outline: 'none',
-                        border: '1px solid',
-                        borderColor: 'blue.500',
-                    }} 
-                    height='34px'
-                    bg='gray.50'
-                />
-            </InputGroup>
-        </Flex>
+      <Flex
+        flexGrow={1}
+        maxWidth={user ? "auto" : "600px"}
+        mr={2}
+        alignItems="center"
+      >
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            color="gray.400"
+            // children={<SearchIcon mb={2} />}
+          >
+            <SearchIcon mb={2} />
+          </InputLeftElement>
+          <Input
+            placeholder="Search Reddit"
+            fontSize="10pt"
+            _placeholder={{ color: "gray.500" }}
+            _hover={{
+              bg: "white",
+              border: "1px solid",
+              borderColor: "blue.500",
+            }}
+            _focus={{
+              outline: "none",
+              border: "1px solid",
+              borderColor: "blue.500",
+            }}
+            height="34px"
+            bg="gray.50"
+          />
+        </InputGroup>
+      </Flex>
     );
-};
-
-export default SearchInput;
+  };
+  export default SearchInput;
