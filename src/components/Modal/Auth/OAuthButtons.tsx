7f47f0c443dfc7115fd6f8f0e568e6b03fa3ad4d
AxOxS <axoxs1@gmail.com>
2023-07-09 14:39:21 +0300

    Custom user properties

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button, Flex, Image, Text } from '@chakra-ui/react';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
-import { auth } from '../../../firebase/clientApp';
+import { auth, firestore } from '../../../firebase/clientApp';
+import { User } from 'firebase/auth';
+import { doc, setDoc } from 'firebase/firestore';
 
 const OAuthButtons:React.FC = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, userCred, loading, error] = useSignInWithGoogle(auth);
+
+    const createUserDocument = async (user: User) => {
+        const userDocRef = doc(firestore, 'users', user.uid);
+        await setDoc(userDocRef, JSON.parse(JSON.stringify(user)));
+    };
+
+    useEffect(() => {
+        if (userCred) {
+            createUserDocument(userCred.user);
+        }
+    }, [userCred]);
+
     return (
         <Flex direction='column' width='100%' mb={4}>
             <Button variant='oauth' mb={2} isLoading={loading} onClick={() => signInWithGoogle()}>
