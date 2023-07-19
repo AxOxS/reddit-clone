350ec5b684627192b708ca23cb6a3f04a723cb92
AxOxS <axoxs1@gmail.com>
2023-07-19 21:17:13 +0300

    Finish RHS home feed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { communityState } from "@/atoms/communitiesAtom";
 import { Post, PostVote } from "@/atoms/postsAtom";
 import CreatePostLink from "@/components/Community/CreatePostLink";
+import PersonalHome from "@/components/Community/PersonalHome";
+import Premium from "@/components/Community/Premium";
+import Recommendations from "@/components/Community/Recommendations";
 import PageContent from "@/components/Layout/PageContent";
 import PostItem from "@/components/Posts/PostItem";
 import PostLoader from "@/components/Posts/PostLoader";
@@ -139,7 +142,11 @@ const Home: NextPage = () => {
           </Stack>
         )}
       </>
-      <></>
+      <Stack spacing={5}>
+        <Recommendations />
+        <Premium />
+        <PersonalHome />
+      </Stack>
     </PageContent>
   );
 };
